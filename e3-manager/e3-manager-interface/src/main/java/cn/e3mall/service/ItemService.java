0aff2866df5bef00d3c9d4e441f0b31efe572aff
hexlong <18634268117@163.com>
2017-09-26 14:37:03 +0800

    序列化pojo 实现后台页面展示分页功能  改造成soa架构

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,8 +1,11 @@
 package cn.e3mall.service;
 
+import cn.e3mall.page.EasyUIDataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 
 	TbItem getItemByid(long itemId);
+	
+	EasyUIDataGridResult getPageExemple(int page,int pageSize);
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.page.EasyUIDataGridResult;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -16,6 +23,22 @@ public class ItemServiceImpl implements ItemService {
 		TbItem item = mapper.selectByPrimaryKey(itemId);
 		
 		return item;
+	}
+	@Override
+	public EasyUIDataGridResult getPageExemple(int page, int pageSize) {
+//		设置当前页数和每页显示条数
+		PageHelper.startPage(page, pageSize);
+//		设置查询条件并执行查询语句
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = mapper.selectByExample(example);
+//		在查询结果中取分页
+		PageInfo info=new PageInfo<>(list);
+//		通过pagebeen封装total和rows数据并返回
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setRows(list);
+		result.setTotal(info.getTotal());
+		
+		return result;
 	}
 
 }
